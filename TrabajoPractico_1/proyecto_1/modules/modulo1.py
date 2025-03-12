d82b4b2a273ddb981f631943c98f41bbdd4bbf73
MarcusEA5 <unacuentaregrosa@gmail.com>
2025-03-12 12:05:22 -0300

    agrego funciones

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,8 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+def funcion4():
+    print('4')
+
+def funcion2():
+    print("Hola Git")
